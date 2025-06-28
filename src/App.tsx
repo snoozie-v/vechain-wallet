352f94f5615f55ca66f7376b8a0761a60111c236
snoozie-v <gettingsleepyz@gmail.com>
2025-06-27 23:27:55 -0500

    adding footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import EnterLotteryForm from './components/EnterLotteryForm';
 import WalletConnection from './components/WalletConnection';
-import LotteryStatus from './components/LotteryStatus';
-
+import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
       <div>
           <WalletConnection />
           <EnterLotteryForm />
+          <Footer />
       </div>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <p>
+        Have fun & Burn SHT 
+    </p>
+    </footer>
+  );
+};
+
+export default Footer;
+
